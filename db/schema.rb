d635a650523e7feb244d08b34e03934535f10ab9
stefanieheinz <stefanieheinz0@gmail.com>
2024-07-09 15:34:03 -0600

    add simple helpers

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_09_210706) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_09_212210) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
     t.decimal "total_gross"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
+    t.date "released_on"
   end
 
 end
